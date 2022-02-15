095c575ad6516fbb6b547780a14e2834386a7a00
Pugianf <pugianf@gmail.com>
2022-02-15 16:25:07 -0300

    update

--- Deforestation_GIZ.py
+++ Deforestation_GIZ.py
@@ -1,13 +1,13 @@
 import xlrd, xlwt
 from xlutils.copy import copy
 
-read_book = xlrd.open_workbook('C:/Users/felipe.jardim/Desktop/Felipe/Dados/Web-Scraping/Pasta1.xls') #Make Readable Copy
+read_book = xlrd.open_workbook('C:/Users/felipe.jardim/Desktop/Felipe/Dados/Web-Scraping/Entwaldungszahlen_Jan_2022.xls') #Make Readable Copy
 write_book = copy(read_book) #Make Writeable Copy
 
 write_sheet1 = write_book.get_sheet(1) #Get sheet 1 in writeable copy
-write_sheet1.write(1, 11, 'test') #Write 'test' to cell (B, 11)
+write_sheet1.write(9, 7, 'test') #Write 'test' to cell (B, 11)
 
-write_sheet2 = write_book.get_sheet(2) #Get sheet 2 in writeable copy
-write_sheet2.write(3, 14, '135') #Write '135' to cell (D, 14)
+write_sheet2 = write_book.get_sheet(1) #Get sheet 2 in writeable copy
+write_sheet2.write(9, 8, 135) #Write 135 to cell (D, 14)
 
-write_book.save('C:/Users/felipe.jardim/Desktop/Felipe/Dados/Web-Scraping/Pasta1.xls')
+write_book.save('C:/Users/felipe.jardim/Desktop/Felipe/Dados/Web-Scraping/Entwaldungszahlen_Jan_2022.xls')
